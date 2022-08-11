9dd60dc6023aafeb15e6b30ac8d3751dd85d0533
Keyno <kofujiw@gmail.com>
2022-08-11 23:15:23 +0900

    function component の実装サンプル

---
+++ src/button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = (props) => {
+    return (
+        <button className="button1">
+            Say, {this.props.hello}
+        </button>
+    )
+}
+
+export default Button;
